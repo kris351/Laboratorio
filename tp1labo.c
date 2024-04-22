89cc488d357ca30f580a07e98a90a4c6da7c129c
kris351 <134452099+kris351@users.noreply.github.com>
2024-04-22 15:42:08 -0300

    Add files via upload

---
+++ tp1labo.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	//diametro*3.14=longitud
+	//3.14*r(elevado)2
+	/*float area, diametro, longitud, pi=3.14, radio, potencia;
+	printf("ingrese el diametro para calcular la longitud:");
+	scanf("%f",&diametro);
+	printf("ingrese el radio para calcular el area:");
+	scanf("%f",&radio);
+	
+	longitud=diametro*pi;
+	potencia= pow(radio,potencia=2);
+	area=(float)pi*potencia;
+	
+	printf("la longitud es: %.2f\n",longitud);
+	printf("el area es: %.2f",area);
+	
+	int num1,num2,num3;
+	float promedio;
+	printf("ingrese el primer numero:");
+	scanf("%d",&num1);
+	printf("ingrese el segundo numero:");
+	scanf("%d",&num2);
+	printf("ingrese el tercer numero:");
+	scanf("%d",&num3);
+	
+	promedio=(num1+num2+num3)/3.0f;
+	
+	printf("los numeros originales son:\n %d \t %d \t %d \n",num1,num2,num3);
+	printf("el promedio de los numeros es:%.2f",promedio);*/
+	
+	int radio, diametro, area;
+	float pi=3.14, longitud;
+	
+	radio=diametro/2.0f;
+	potencia=pow(radio,potencia=2);
+	area=pi*potencia;
+	diametro=radio*2.0f;
+	longitud=diametro*pi;
+	
+	printf("")
+	
+	
+	
+	return 0;
+}
